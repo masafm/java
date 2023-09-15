e336d9bd5b958ec114fff77d4e56a317249fa8bd
masafm <masa@mkashi.com>
2023-09-15 20:00:50 +0900

    update

--- jmx-test/Main.java
+++ jmx-test/Main.java
@@ -1,6 +1,10 @@
+import java.net.URL;
+import java.net.HttpURLConnection;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import sample.Sample;
-
+    
 class Main {
     private static ArrayList<Integer> list = new ArrayList<>();
     public static void main(String[] args) {
@@ -10,6 +14,24 @@ class Main {
 	    try {
 		Main.myFunc();
 		s.print();
+
+		try {
+		    // URLを作成してGET通信を行う
+		    URL url = new URL("http://192.168.32.100:11080/");
+		    HttpURLConnection http = (HttpURLConnection)url.openConnection();
+		    http.setRequestMethod("GET");
+		    http.connect();
+		    // サーバーからのレスポンスを標準出力へ出す
+		    BufferedReader reader = new BufferedReader(new InputStreamReader(http.getInputStream()));
+		    String xml = "", line = "";
+		    while((line = reader.readLine()) != null)
+			xml += line;
+		    System.out.println(xml);
+		    reader.close();
+		} catch(Exception e) {
+		    System.out.println("Exception!!!");
+		}
+		
 		Thread.sleep(1000);
 		for(int i=0; i<1000; i++) {
 		    list.add(0,new Integer(i));
