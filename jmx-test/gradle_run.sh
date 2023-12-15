c7d28fc16b394862ecc2f55ad94b7f372d260075
masafm <masa@mkashi.com>
2023-12-15 19:24:56 +0900

    update

--- jmx-test/gradle_run.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/bash
-JAVA_OPTS="-Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.port=9012 -Dcom.sun.management.jmxremote.authenticate=false -javaagent:/opt/dd-java-agent.jar -Ddd.profiling.enabled=true -XX:FlightRecorderOptions=stackdepth=256 -Ddd.logs.injection=true -Ddd.trace.debug=false -Ddd.trace.config=datadog.properties" gradle run
