42282685cf745f71d64bb419688f26e762e22973
masafm <masa@mkashi.com>
2023-12-15 10:54:39 +0900

    update

--- jmx-test/app/src/main/java/jmx/test/Main.java
+++ jmx-test/app/src/main/java/jmx/test/Main.java
@@ -6,23 +6,19 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import jmx.test.sample.Sample;
+import datadog.trace.api.DDTags;
+import datadog.trace.api.Trace;
+import io.opentracing.Scope;
 import io.opentracing.Span;
 import io.opentracing.Tracer;
 import io.opentracing.util.GlobalTracer;
 
 class Main {
     private static ArrayList<Integer> list = new ArrayList<>();
+
+    //@Trace(operationName = "main", resourceName = "jmx.test.Main.main")
     public static void main(String[] args) {
 	System.out.println("main method(String[]).");
-	final Span span = GlobalTracer.get().activeSpan();
-        if (span != null) {
-	    int customer_id = 254889;
-	    String customer_tier = "platinum";
-	    int cart_value = 867;
-          span.setTag("customer.id", customer_id);
-          span.setTag("customer.tier", customer_tier);
-          span.setTag("cart.value", cart_value);
-        }
 	Sample s = new Sample();
 	Main m = new Main();
 	while (true) {
@@ -47,7 +43,18 @@ class Main {
 		} catch(Exception e) {
 		    System.out.println("Exception!!!");
 		}
-		
+
+		final Span span = GlobalTracer.get().activeSpan();
+		if (span != null) {
+		    System.out.println("Custom span tag");
+		    int customer_id = 254889;
+		    String customer_tier = "platinum";
+		    int cart_value = 867;
+		    span.setTag("customer.id", customer_id);
+		    span.setTag("customer.tier", customer_tier);
+		    span.setTag("cart.value", cart_value);
+		}
+
 		Thread.sleep(1000);
 		for(int i=0; i<1000; i++) {
 		    list.add(0,Integer.valueOf(i));
--- jmx-test/gradle_run.sh
+++ jmx-test/gradle_run.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/bash
-project_root=$(dirname $0)
-( cd "${project_root}"; /usr/bin/java \
-	       -Dcom.sun.management.jmxremote.ssl=false \
+JAVA_OPTS="-Dcom.sun.management.jmxremote.ssl=false \
 	       -Dcom.sun.management.jmxremote.port=9012 \
 	       -Dcom.sun.management.jmxremote.authenticate=false \
 	       -javaagent:/opt/dd-java-agent.jar \
 	       -Ddd.profiling.enabled=true -XX:FlightRecorderOptions=stackdepth=256 \
 	       -Ddd.logs.injection=true \
 	       -Ddd.trace.debug=false \
-	       -Ddd.trace.config=${project_root}/datadog.properties \
-	       -jar ./app/build/libs/app-all.jar jmx.test.Main
-)
+	       -Ddd.trace.config=${project_root}/datadog.properties" \
+gradle run
