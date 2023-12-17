deab4af8aaf0045c122262dfb4328eff274f41f3
masafm <masa@mkashi.com>
2023-12-18 08:15:50 +0900

    update

--- jmx-test/run.sh
+++ jmx-test/run.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/bash
 ( cd "$(dirname $0)"; /usr/bin/java \
-	       -Dcom.sun.management.jmxremote.ssl=false \
-	       -Dcom.sun.management.jmxremote.port=9012 \
-	       -Dcom.sun.management.jmxremote.authenticate=false \
-	       -javaagent:/opt/dd-java-agent.jar \
-	       -Ddd.profiling.enabled=true -XX:FlightRecorderOptions=stackdepth=256 \
-	       -Ddd.logs.injection=true \
-	       -Ddd.trace.debug=false \
-	       -Ddd.trace.config=${project_root}/app/datadog.properties \
-	       -jar ./app/build/libs/app-all.jar jmx.test.Main
+			  -Dcom.sun.management.jmxremote.ssl=false \
+			  -Dcom.sun.management.jmxremote.port=9012 \
+			  -Dcom.sun.management.jmxremote.authenticate=false \
+			  -javaagent:/opt/dd-java-agent.jar \
+			  -Ddd.profiling.enabled=true -XX:FlightRecorderOptions=stackdepth=256 \
+			  -Ddd.logs.injection=true \
+			  -Ddd.trace.debug=false \
+			  -Ddd.trace.config="$(dirname $0)/app/datadog.properties" \
+			  -jar ./app/build/libs/app-all.jar jmx.test.Main
 )
