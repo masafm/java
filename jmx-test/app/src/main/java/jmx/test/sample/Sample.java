5e2d5e7c23704dd26559f9974feab6f9689e6cd6
masafm <masa@mkashi.com>
2023-12-18 10:45:35 +0900

    update

--- jmx-test/app/src/main/java/jmx/test/sample/Sample.java
+++ jmx-test/app/src/main/java/jmx/test/sample/Sample.java
@@ -1,10 +1,17 @@
 package jmx.test.sample;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+// logback
+// import org.slf4j.Logger;
+// import org.slf4j.LoggerFactory;
+// log4j2
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Sample {
-    private final Logger logger = LoggerFactory.getLogger(getClass());
+    // logback
+    //private final Logger logger = LoggerFactory.getLogger(getClass());
+    // log4j2
+    private final Logger logger = LogManager.getLogger();
     
     public void print(){
 	logger.info("sample.Sample print()");
