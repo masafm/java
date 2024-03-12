992e2cfa876d84a26aee5da4677eb90be0304b56
masafm <masa@mkashi.com>
2024-03-12 11:44:47 +0900

    update

--- tomcat/app/src/main/java/com/ddmasa/tomcat/SampleController.java
+++ tomcat/app/src/main/java/com/ddmasa/tomcat/SampleController.java
@@ -14,6 +14,8 @@ import io.opentracing.Span;
 import io.opentracing.Tracer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 @RestController
 public class SampleController {
@@ -36,6 +38,20 @@ public class SampleController {
 	Main m = new Main();
 	m.exec();
         return "Hello World!";
+    }
+    
+    @Trace(operationName = "exception", resourceName = "com.ddmasa.tomcat.sample.SampleController")
+    @RequestMapping("/exception")
+    public String exception() throws Exception {
+	logger.info("exception");
+        throw new Exception();
+    }
+    
+    @Trace(operationName = "500", resourceName = "com.ddmasa.tomcat.sample.SampleController")
+    @RequestMapping("/500")
+    public ResponseEntity<String> httpStatus500() {
+	logger.info("http_status_500");
+        return new ResponseEntity<>("Internal Server Error Message", HttpStatus.INTERNAL_SERVER_ERROR);
     }
     
     @Trace(operationName = "helloSp", resourceName = "com.ddmasa.tomcat.sample.SampleController")
